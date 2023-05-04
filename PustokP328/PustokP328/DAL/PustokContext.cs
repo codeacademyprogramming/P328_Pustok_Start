07e5c560bdc83d58d5c94855e04dc942090722be
Elchin Aghayev <Elchin@code.local>
2023-05-04 16:32:15 +0400

    project added

---
+++ PustokP328/PustokP328/DAL/PustokContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PustokP328.Models;
+
+namespace PustokP328.DAL
+{
+    public class PustokContext:DbContext
+    {
+        public PustokContext(DbContextOptions<PustokContext> options):base(options) { }
+
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
---
+++ PustokP328/PustokP328/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PustokP328.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
